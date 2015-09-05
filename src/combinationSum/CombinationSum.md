b1278d47e42c0a1541610d1f0a095fbda5bf9e5f
xingcan <xccui@Xingcans-MacBook-Pro.local>
2015-09-05 20:27:09 +0800

    CombinationSum

---
+++ src/combinationSum/CombinationSum.md
@@ -0,0 +1,16 @@
+Given a set of candidate numbers (_**C**_) and a target number (_**T**_), find all unique combinations in _**C**_ where the candidate numbers sums to _**T**_.
+
+The **same** repeated number may be chosen from C unlimited number of times.
+
+**Note:**
+
+* All numbers (including target) will be positive integers.
+* Elements in a combination (_a1, a2, … , ak_) must be in non-descending order. (ie, _a1 ≤ a2 ≤ … ≤ ak_).
+* The solution set must not contain duplicate combinations.
+
+For example, given candidate set `2,3,6,7` and target `7`, 
+
+A solution set is: 
+
+	[7] 
+	[2, 2, 3] 
---
+++ src/combinationSum/Soluction.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by xccui on 9/5/15.
+ */
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> resultList = new LinkedList<List<Integer>>();
+        Arrays.sort(candidates);
+        List<Integer> currentList;
+        for (int i = 0; i < candidates.length; i++) {
+            currentList = new LinkedList<Integer>();
+            tryToCombine(resultList, candidates, i, currentList, 0, target);
+        }
+        return resultList;
+    }
+
+    public boolean tryToCombine(List<List<Integer>> resultList, int[] candidates, int index, List<Integer> currentList, int sum, int target) {
+        currentList.add(candidates[index]);
+        sum += candidates[index];
+        //System.out.println(currentList + "\t" + sum);
+        if (sum == target) {
+            resultList.add(new ArrayList<Integer>(currentList));
+        } else if (sum < target) {
+            boolean flag = true;
+            for (int i = index; i < candidates.length && flag; i++) {
+                flag = tryToCombine(resultList, candidates, i, currentList, sum, target);
+            }
+        }
+        currentList.remove(currentList.size() - 1);
+        return sum < target;
+    }
+
+    public static void main(String args[]) {
+        CombinationCode solution = new CombinationCode();
+        System.out.println(solution.combinationSum(new int[]{2, 3, 6, 7}, 7));
+    }
+}
